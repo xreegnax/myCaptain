efe992b85ec2c221e0fef12227885103c251c56c
xreegnax <67576156+xreegnax@users.noreply.github.com>
2021-06-20 23:17:03 +0530

    Create Positive.py

---
+++ Positive.py
@@ -0,0 +1,20 @@
+list1 = [12, -7, 5, 64, -14]
+print("List 1:")
+print (list1)
+print("")
+print("Positive numbers in the List2:")
+for num in list1:
+    if num >= 0:
+       print(num)
+
+print("")
+print("")
+       
+list2 = [12, 14, -95, 3]
+print("List 2:")
+print (list2)
+print("")
+print("Positive numbers in the List2:")
+for num in list2:
+    if num >= 0:
+       print(num)
